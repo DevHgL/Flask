67fafa87404f14f0b07b7d8ecde024499ad22114
DevHgL <hleomelo1@icloud.com>
2023-09-17 23:05:07 -0300

    Teste 2

--- teste.py
+++ teste.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+
+def test_main(nome,sobrenome,idade):
+  nome 
+  sobrenome
+  idade
+  
+  print("Olá, meu nome é " + nome + sobrenome + " e possuo  " + idade + "anos")
+  
+test_main("Gabi","Quadra",22)
+test_main("Hugo","Leo",22)
